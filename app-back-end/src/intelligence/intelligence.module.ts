9d85cd41edd1a3ecc4c31e51bf9e070aec6ce2aa
Renato Vilasboas <renato.vilasboas@terra.com.br>
2025-12-10 09:16:56 -0300

    cleanup: Remove all unnecessary files and directories
    - Remove old project files (app-back-end, app-front-end, backend, frontend)
    - Remove documentation files (docs/)
    - Remove configuration files (configs/)
    - Remove test files (e2e-tests/)
    - Remove scripts directory
    - Keep only README.md for clean BIB platform repository

--- app-back-end/src/intelligence/intelligence.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IntelligenceController } from './intelligence.controller';
-import { IntelligenceService } from './intelligence.service';
-
-@Module({
-    controllers: [IntelligenceController],
-    providers: [IntelligenceService]
-})
-export class IntelligenceModule { }
--- backend/src/loan/loan.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LoanController } from './loan.controller';
-import { LoanService } from './loan.service';
-
-@Module({
-    controllers: [LoanController],
-    providers: [LoanService]
-})
-export class LoanModule { }
